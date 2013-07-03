9b90c94b921ab9d8dc1705d1d34a9bae7e205d09
Nick Braica <nick.braica@gmail.com>
2013-07-03 12:51:18 -0600

    changed wordpress links to be root relative

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
       <ul id="navigation">
         <li class="about-link"><a href="about.php">More Info</a></li>
         <li class="portfolio-link"><a href="portfolio.php">The Work</a></li>
-        <li class="blog-link"><a href="blog">My Thoughts</a></li>
+        <li class="blog-link"><a href="blog/">My Thoughts</a></li>
         <li class="contact-link"><a href="contact.php">Let's Talk</a></li>
       </ul>
       <div class="clear"></div>
