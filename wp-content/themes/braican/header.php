0a35e745baf0515d6ca1580934e679510f40a6c2
Nick Braica <nbraica@tankdesign.com>
2014-03-19 15:33:29 -0400

    favicon

--- wp-content/themes/braican/header.php
+++ wp-content/themes/braican/header.php
@@ -14,6 +14,8 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
 <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
