37ccba5db8aa6c0f2496974d91231f02605c5ba8
braican <nick.braica@gmail.com>
2014-03-18 21:52:45 -0400

    404 page is awesome

--- wp-content/themes/braican/404.php
+++ wp-content/themes/braican/404.php
@@ -8,27 +8,21 @@
 get_header(); ?>
 
 	
-	<div id="content" class="site-content notfound" role="main">
-		
-		<div class="braica-container br-cf">
-			<article id="post-0" class="post not-found">
+	<div id="content" class="site-content" role="main">
+		<section class="notfound crazy-air">
+			<article id="post-0" class="post not-found braica-container">
 				<div class="braica-block">
 					<header class="entry-header">
 						<h1 class="entry-title"><?php _e( "Uh oh, you're looking for something that doesn't exist.", 'braican' ); ?></h1>
 					</header><!-- .entry-header -->
 				</div>
-				
-				<div class="col col2">
-					<div class="entry-content braica-block">
+				<div class="entry-content braica-block br-cf">
+					<div class="col col2">
 						<p><?php _e( 'Sorry about that. As consolation, I hope you enjoy this photo of me getting wicked air on a snow tube.', 'braican' ); ?></p>
-					</div><!-- .entry-content -->
-				</div>
-
-				<div class="col col4">
-					<div class="img-container"></div>
-				</div>
-			</article><!-- #post-0 .post .error404 .not-found -->
-		</div>
+					</div>
+				</div><!-- .entry-content -->
+			</article>	
+		</section>
 
 	</div><!-- #content -->
 
