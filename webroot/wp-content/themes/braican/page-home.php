e016d5c5a7b0dd952a1578b1945558a66d50bab4
Nick Braica <nick.braica@gmail.com>
2017-12-28 21:35:54 -0500

    project gallery, akismet update

--- webroot/wp-content/themes/braican/page-home.php
+++ webroot/wp-content/themes/braican/page-home.php
@@ -39,23 +39,25 @@ get_header(); ?>
 			<section class="projectgallery">
 				<?php while($projectQuery->have_posts()) : $projectQuery->the_post() ?>
 					<?php if(has_post_thumbnail()) : ?>
-						<div class="projectgallery__thumb">
-							<a
-								class="projectgallery__link"
-								href="<?php the_permalink() ?>"
-								title="<?php the_title(); ?>"
-								style="background-image:url(<?php echo get_the_post_thumbnail_url() ?>)"
-							></a>
+						<a class="projectgallery__thumb" href="<?php the_permalink() ?>">
+							<div class="projectgallery__wrapper">
+								<?php the_post_thumbnail('full'); ?>
 
-							<a class="projectgallery__details" href="<?php the_permalink(); ?>">
-								<div class="projectgallery__wrap">
-									<?php the_title('<h3 class="projectgallery__title">', '</h3>'); ?>
-									<?php sk_the_field('braican_project_excerpt', array('before' => '<div class="projectgallery__excerpt">', 'after' => '</div>')); ?>
+								<div class="projectgallery__details">
+									<div class="projectgallery__wrap">
+										<?php the_title('<h3 class="projectgallery__title">', '</h3>'); ?>
+										<?php sk_the_field('braican_project_excerpt', array('before' => '<div class="projectgallery__excerpt">', 'after' => '</div>')); ?>
+									</div>
 								</div>
-							</a>
-						</div>
+							</div>
+						</a>
 					<?php endif; ?>
 				<?php endwhile; ?>
+				<a href="#" class="projectgallery__thumb projectgallery__thumb--promo">
+					<div class="projectgallery__wrapper">
+						<p class="projectgallery__promo">Let's get going on your project</p>
+					</div>
+				</a>
 			</section>
 		<?php endif; ?>
 	</div><!-- #primary -->
