4f554c4bd80b0a4ebd403021d11ecd7364a3f817
Nick Braica <nick.braica@gmail.com>
2018-06-15 17:10:43 -0400

    some layout styling

--- webroot/wp-content/themes/braican/js/build/production.js
+++ webroot/wp-content/themes/braican/js/build/production.js
@@ -1,91 +1,22 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-
-function elYPosition(el) {
+/**
+ * Get the Y position of the element we're scrolling to.
+ * @param {Element} el What we're trying to get the Y position of
+ */
+var elYPosition = function elYPosition(el) {
     var y = el.offsetTop;
     var node = el;
     while (node.offsetParent && node.offsetParent !== document.body) {
         node = node.offsetParent;
         y += node.offsetTop;
     }return y;
-}
-
-// function scrollIt(yPos) {
-//     window.scrollTo(0, yPos);
-// }
-
-// function smoothScroll(el) {
-//     const startY = window.pageYOffset;
-//     const stopY = elmYPosition(el);
-//     const distance = stopY > startY ? stopY - startY : startY - stopY;
-//     if (distance < 100) {
-//         window.scrollTo(0, stopY); return;
-//     }
-//     let speed = Math.round(distance / 100);
-//     if (speed >= 40) speed = 40;
-//     const step = Math.round(distance / 25);
-//     let leapY = stopY > startY ? startY + step : startY - step;
-//     let timer = 1;
-//     if (stopY > startY) {
-//         for (let i = startY; i < stopY; i += step) {
-//             console.log(leapY);
-//             // window.scrollTo(0, leapY);
-//             setTimeout(() => {
-//                 scrollIt(leapY);
-//             }, timer * speed);
-//             leapY += step;
-//             if (leapY > stopY) leapY = stopY;
-//             timer += 1;
-//         }
-//         return;
-//     }
-//     for (let i = startY; i > stopY; i -= step) {
-//         // setTimeout(window.scrollTo(0, leapY), timer * speed);
-//         // setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
-//         leapY -= step;
-//         if (leapY < stopY) leapY = stopY;
-//         timer += 1;
-//     }
-// }
-
-/**
- * Loop scrolling animation
- */
-// function loopAnimateScroll(timestamp) {
-
-//     pattern = percentage < 0.5 ? 4 * percentage * percentage * percentage : (percentage - 1) * (2 * percentage - 2) * (2 * percentage - 2) + 1; // acceleration until halfway, then deceleration
-
-//     if (!start) { start = timestamp; }
-//     timeLapsed += timestamp - start;
-//     percentage = (timeLapsed / parseInt(animateSettings.speed, 10));
-//     percentage = (percentage > 1) ? 1 : percentage;
-//     position = startLocation + (distance * easingPattern(animateSettings, percentage));
-//     window.scrollTo(0, Math.floor(position));
-//     if (!stopAnimateScroll(position, endLocation)) {
-//         window.requestAnimationFrame(loopAnimateScroll);
-//         start = timestamp;
-//     }
-// };
-
-// function smoothScrollTo(el) {
-//     const startY = window.pageYOffset;
-//     const stopY = elYPosition(el);
-
-//     let timer = 0;
-
-//     for (let i = startY; i < stopY; i += 4) {
-//         setTimeout(() => {
-//             window.scrollTo(0, i);
-//         }, timer * 2);
-
-//         timer += 1;
-//     }
-// }
+};
 
 /**
  * Determine the document's height
