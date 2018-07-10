816afd070fc473e660a9479f5fa443d8a3490ba4
Nick Braica <nick.braica@gmail.com>
2018-07-10 15:58:35 -0400

    Ajax the contact form

--- src/js/modules/contactform.js
+++ src/js/modules/contactform.js
@@ -5,45 +5,39 @@ export default function contactform() {
         return false;
     }
 
-    // const messageEl = document.createElement('p');
-    // messageEl.classList.add('submission__message');
-    // const message = {
-    //     success: "Thanks for the note! I'll get back to you as soon as I can.",
-    //     error:
-    //         'There was an error submitting the form. You can always send me a note at <a href="mailto:nick.braica@gmail.com">nick.braica@gmail.com</a>, or try again later.',
-    // };
+    const messageEl = document.createElement('p');
+    const message = {
+        success: "Thanks for the note! I'll get back to you as soon as I can.",
+        error:
+            'There was an error submitting the form. You can always send me a note at <a href="mailto:nick.braica@gmail.com">nick.braica@gmail.com</a>, or try again later.',
+    };
 
-    // $form.addEventListener('submit', (e) => {
-    //     e.preventDefault();
-    //     const request = new XMLHttpRequest();
-    //     const formData = new FormData($form);
+    messageEl.classList.add('submission__message');
 
-    //     request.open('POST', $form.getAttribute('action'), true);
-    //     request.setRequestHeader(
-    //         'Content-Type',
-    //         'application/x-www-form-urlencoded; charset=UTF-8'
-    //     );
-    //     request.send(formData);
+    $form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const request = new XMLHttpRequest();
+        const formData = new FormData($form);
 
-    //     // remove the message
-    //     if (messageEl.parentNode) {
-    //         messageEl.parentNode.removeChild(messageEl);
-    //     }
+        request.open('POST', $form.getAttribute('action'), true);
+        request.setRequestHeader(
+            'Content-Type',
+            'application/x-www-form-urlencoded; charset=UTF-8'
+        );
+        request.send(formData);
 
-    //     request.onreadystatechange = () => {
-    //         // <4 =  waiting on response from server
-    //         if (request.readyState < 4) {
-    //             console.log('loading');
-    //         }
-    //         // 4 = Response from server has been completely loaded.
-    //         else if (request.readyState === 4) {
-    //             console.log(request);
+        // remove the message
+        if (messageEl.parentNode) {
+            messageEl.parentNode.removeChild(messageEl);
+        }
 
-    //             // 200 - 299 = successful
-    //             messageEl.innerHTML =
-    //                 request.status == 200 && request.status < 300 ? message.success : message.error;
-    //             $form.parentNode.appendChild(messageEl);
-    //         }
-    //     };
-    // });
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                // 200 - 299 = successful
+                messageEl.innerHTML =
+                    request.status == 200 && request.status < 300 ? message.success : message.error;
+                $form.parentNode.appendChild(messageEl);
+            }
+        };
+    });
 }
