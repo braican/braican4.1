8a155a2b2be92a0c0c5396bab2bd550b5844c70c
Nick Braica <nick.braica@gmail.com>
2019-08-18 09:54:14 -0700

    Better footer and type

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './Footer.module.scss';
+
 const Footer = ({ className }) => (
   <footer className={className}>
-    <p>&copy; {new Date().getFullYear()} Nick Braica</p>
+    <p className={styles.copyright}>&copy; {new Date().getFullYear()} Nick Braica</p>
+
+    <ul className={styles.social}>
+      <li>
+        <a href="https://github.com/braican" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/braican/" target="_blank" rel="noopener noreferrer">
+          Instagram
+        </a>
+      </li>
+      <li>
+        <a href="https://untappd.com/user/braican" target="_blank" rel="noopener noreferrer">
+          Untappd
+        </a>
+      </li>
+    </ul>
   </footer>
 );
 
--- wp/themes/braican/footer.php
+++ wp/themes/braican/footer.php
@@ -14,9 +14,9 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer l-container">
-		
+
 		<?php echo do_shortcode('[contact-form-7 id="315" title="Contact form 1"]'); ?>
-		
+
 		<div class="site-info">
 			<p>&copy; <?php echo date('Y'); ?> | braican</p>
 		</div><!-- .site-info -->
