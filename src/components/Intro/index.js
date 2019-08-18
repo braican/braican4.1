f9365ed2a3f43fc4b42a7aa10cc2ea924e9f9902
Nick Braica <nick.braica@gmail.com>
2019-08-18 12:01:09 -0700

    Cleanup

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './Intro.module.scss';
-
 const Intro = ({ body }) => <div className="stack" dangerouslySetInnerHTML={{ __html: body }} />;
 
 Intro.propTypes = {
