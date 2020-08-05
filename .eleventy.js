28135aeefe193707cc0fb974644da5faf5fa83ca
Nick Braica <nick.braica@gmail.com>
2020-08-05 16:13:48 -0700

    Scaffold things

---
+++ .eleventy.js
@@ -0,0 +1,36 @@
+const MarkdownIt = require('markdown-it');
+const CleanCSS = require('clean-css');
+
+const md = new MarkdownIt();
+
+module.exports = function (eleventyConfig) {
+  // A useful way to reference the context we are runing eleventy in
+  const env = process.env.ELEVENTY_ENV;
+
+  eleventyConfig.addFilter('markdown_it', function (value) {
+    return md.render(value);
+  });
+
+  eleventyConfig.addFilter('cssmin', function (code) {
+    return new CleanCSS({
+      sourceMap: true,
+    }).minify(code).styles;
+  });
+
+  // pass some assets right through
+  eleventyConfig.addPassthroughCopy('./src/fonts');
+
+  // For sourcemaps, only in development.
+  if (env === 'development') {
+    eleventyConfig.addPassthroughCopy('./src/styles');
+    eleventyConfig.addPassthroughCopy({
+      './src/_includes/build/main.css.map': 'styles/main.css.map',
+    });
+  }
+
+  return {
+    dir: {
+      input: 'src',
+    },
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,13 @@
+---
+layout: layouts/base.njk
+lede: Let's build something.
+
+main: |
+  I'm a design-focused developer who has over nine years of experience working directly with people to build strategies and impactful online experiences for their brands.
+
+  Full time, I spend my days leading technology at [Upstatement](https://upstatement.com), a digital studio based in Boston. I live in Portland, Oregon because the skiing is better out here.
+
+  Sometimes I make my own beer and it usually comes out alright. I also have a tendency to yell at Boston sports teams.
+
+promo: I'm available to work with you on your next thing. Give me a shout.
+---
